0d8e8e5425d4f8f9b94abd67395f71ab4d1ebc59
David Thomason <dtom90@users.noreply.github.com>
2020-10-18 08:29:27 -0600

    Bumped hello-watson

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ const {AssistantApp} = require('hello-watson/svelte');
 const app = new AssistantApp({
   target: document.body,
   props: {
-    title: 'Sonny Weather Bot'
+    title: 'Sonny Weather Bot',
+    titleColor: 'blue'
   }
 });
 
