926631182c1e3db74d9e79a0f32ce20d2f811d40
David Thomason <dtom90@users.noreply.github.com>
2020-11-20 20:47:44 -0700

    Enabled weather bot

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const {assistantServer, handler} = require('hello-watson');
+const {getWeather} = require('./lib/openWeatherClient');
 
 const PORT = 3000;
 
-const postProcess = (result) => {
-  if (result.context.skills['main skill'].user_defined) {
+const postProcess = async (result) => {
+  const context = result.context.skills['main skill'].user_defined;
+  if (context && context.condition && context.city) {
+    const weather = await getWeather(context.city);
+    
+    let weatherResponse;
+    if (context.condition === 'Weather') {
+      weatherResponse = `The weather in ${context.city} is currently ${weather.weather[0].description} with a temperature of ${Math.round(weather.main.temp - 273.15)} degrees Celsius`;
+    } else {
+      weatherResponse = 'I\'m sorry, I did not recognize the condition ' + context.condition;
+    }
+    
     result.output.generic.push({
       response_type: 'text',
-      text: Object.entries(result.context.skills['main skill'].user_defined).map(([k, v]) => k + ': ' + v).join(', ')
+      text: weatherResponse
     });
   }
 };
