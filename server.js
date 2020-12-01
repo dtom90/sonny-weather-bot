da3730527b67b143db70828d9ba75b3ffdff3dcf
David Thomason <dtom90@users.noreply.github.com>
2020-11-30 21:42:23 -0700

    Improvements to condition and error handling

--- server.js
+++ server.js
@@ -6,14 +6,19 @@ const PORT = 3000;
 const postProcess = async (result) => {
   const context = result.context.skills['main skill'].user_defined;
   if (context && context.condition && context.city) {
-    const weather = await getWeather(context.city);
-    
-    const weatherResponse = composeResponse(weather, context);
-    
-    result.output.generic.push({
-      response_type: 'text',
-      text: weatherResponse
-    });
+    try {
+      const weather = await getWeather(context.city);
+      
+      result.output.generic.push({
+        response_type: 'text',
+        text: composeResponse(weather, context)
+      });
+    } catch (e) {
+      result.output.generic.push({
+        response_type: 'text',
+        text: `I'm sorry, I could not find weather conditions for ${context.city}.`
+      });
+    }
   }
 };
 
